be6c3a57c6868b30090e1a30dd3e7bc167cf6019
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-13 22:29:21 +0530

    added delete method and fixed bugs

--- index.js
+++ index.js
@@ -22,10 +22,7 @@ app.get('/api/courses', (req, res) => {
 
 app.post('/api/courses', (req, res) => {
     const { error } = validateCourse(req.body); // result.error
-    if(error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if(error) return res.status(400).send(error.details[0].message);
 
     const course = {
         id: courses.length + 1,
@@ -38,14 +35,11 @@ app.post('/api/courses', (req, res) => {
 app.put('/api/courses/:id', (req, res) => {
     //find the course
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('The course with given id was not found');
+    if(!course) return res.status(404).send('The course with given id was not found');
 
     // validate the course
     const { error } = validateCourse(req.body); // result.error
-    if(error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if(error) return res.status(400).send(error.details[0].message);
 
     //update the course
     course.name = req.body.name;
@@ -62,10 +56,21 @@ function validateCourse(course) {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('The course with given id was not found');
+    if(!course) return res.status(404).send('The course with given id was not found');
     res.send(course);
 
 });
+
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with given id was not found');
+
+    const index = courses.indexOf(course)
+    courses.splice(index, 1);
+
+    res.send(courses);
+})
 
 
 const port = process.env.PORT || 3000;
